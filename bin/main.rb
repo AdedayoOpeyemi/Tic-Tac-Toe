cd9174bcb5d901a55d4bb63aa7726f669b18166e
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-05-19 21:51:39 +0100

    Fixed not running

--- bin/main.rb
+++ bin/main.rb
@@ -30,37 +30,40 @@ def win_check(win_combination, player_record)
   end
 end
 
-def Game_Cycle
 
-    puts 'Player 1 please input your name'
-    name = gets.strip
-    token = 'X'
-    player_1 |= Player.new(name, token)
-    puts 'Player 2 please input your name'
-    name = gets.strip
-    token = 'O'
-    player_2 |= Player.new(name, token)
-    game_board = Board.new
+
+  puts 'Player 1 please input your name'
+  name = gets.strip
+  token = 'X'
+  player_1 = Player.new(name, token)
+  puts 'Player 2 please input your name'
+  name = gets.strip
+  token = 'O'
+  player_2 = Player.new(name, token)
+  game_board = Board.new
 
   win_combination = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
   count = 1
   game_on = true
 
-  while (game_on = true)
-    puts board.display_board
-    current_player = (count % 2).odd? ? player_1 : player_2
+  while (game_on == true)
+    puts game_board.display_board
+    if (count % 2).odd?
+        current_player = player_1
+    else
+        current_player = player_2
+    end
     puts "#{current_player.name} it is your turn, please make your move"
     move = gets.strip
 
-    if move != board.board[move.to_i - 1] && move != [/\d/]
+    if move != game_board.board[move.to_i - 1].to_s # && /\d/.~match(move) 
       puts 'Move is not valid, Make a valid move'
     end
 
     current_player.record << move
-    board.board[move.to_i - 1] = current_player.sign
-    if win_check(win_combination, current_player.record) then game_on = false
+    game_board.board[move.to_i - 1] = current_player.sign
+    #if win_check(win_combination, current_player.record) 
+      #game_on = false
+    #end
     count += 1
   end
-end
-
-Game_Cycle
