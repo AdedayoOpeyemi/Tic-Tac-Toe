25686a33516cf6c3d4b08da8d8c41d1a5bd945fc
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-05-20 12:38:41 +0100

    fixed compilation errors

--- bin/main.rb
+++ bin/main.rb
@@ -30,40 +30,38 @@ def win_check(win_combination, player_record)
   end
 end
 
-
-
-  puts 'Player 1 please input your name'
-  name = gets.strip
-  token = 'X'
-  player_1 = Player.new(name, token)
-  puts 'Player 2 please input your name'
-  name = gets.strip
-  token = 'O'
-  player_2 = Player.new(name, token)
-  game_board = Board.new
-
-  win_combination = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-  count = 1
-  game_on = true
-
-  while (game_on == true)
-    puts game_board.display_board
-    if (count % 2).odd?
-        current_player = player_1
-    else
-        current_player = player_2
-    end
-    puts "#{current_player.name} it is your turn, please make your move"
-    move = gets.strip
-
-    if move != game_board.board[move.to_i - 1].to_s # && /\d/.~match(move) 
-      puts 'Move is not valid, Make a valid move'
-    end
-
-    current_player.record << move
-    game_board.board[move.to_i - 1] = current_player.sign
-    #if win_check(win_combination, current_player.record) 
-      #game_on = false
-    #end
-    count += 1
+puts 'Player 1 please input your name'
+name = gets.strip
+token = 'X'
+Player_1 = Player.new(name, token)
+puts 'Player 2 please input your name'
+name = gets.strip
+token = 'O'
+Player_2 = Player.new(name, token)
+game_board = Board.new
+# win_combination = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+count = 1
+game_on = true
+while game_on
+  puts game_board.display_board
+  current_player = if count.odd?
+                     Player_1
+                   else
+                     Player_2
+                   end
+  puts "#{current_player.name} it is your turn, please make your move"
+  move = gets.strip
+  gamecheck = game_board.board[move.to_i - 1]
+  puts 'Move is not valid, Make a valid move' unless move.to_i == gamecheck
+  current_player.record << move.to_i
+  game_board.board[move.to_i - 1] = current_player.sign
+  #   if win_check(win_combination, current_player.record)
+  #     puts '#{current_player.name} is the winner for this round'
+  #     game_on = false
+  #   end
+  count += 1
+  if count == 10
+    puts 'The game is a draw'
+    break
   end
+end
