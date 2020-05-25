af0f7e58ec0751ce4d909172cad5a630d8cc74b3
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-05-25 20:37:20 +0100

    Added player class to lib

---
+++ lib/players.rb
@@ -0,0 +1,10 @@
+class Player
+    attr_reader :name, :sign
+    attr_accessor :record
+  
+    def initialize(name, sign)
+      @name = name
+      @sign = sign
+      @record = []
+    end
+  end
