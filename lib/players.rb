ffcdb2a5e3a37e2db65b07c77f5a1997da2d23c3
moma <angelmomaa@gmail.com>
2020-05-26 10:43:44 -0700

    removed chunk of code inside plaers.rb

--- lib/players.rb
+++ lib/players.rb
@@ -9,12 +9,3 @@ class Player
   end
   
 end
-
-  puts 'Player 1 please input your name'
-  name = gets.strip
-  token = 'X'
-  player_1 = Player.new(name, token)
-  puts 'Player 2 please input your name'
-  name = gets.strip
-  token = 'O'
-  player_2 = Player.new(name, token)
