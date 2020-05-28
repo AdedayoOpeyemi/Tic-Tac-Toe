8c56da7d3996b840f9670c3b60cde1ef78d4c50e
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-05-28 19:51:57 +0100

    Refactored code

--- bin/main.rb
+++ bin/main.rb
@@ -2,100 +2,44 @@
 
 require_relative '../lib/players'
 require_relative '../lib/game_status'
-
-class Board
-  attr_reader :board
-
-  def initialize
-    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @count = 1
-    @game_status = GameStatus.new
-  end
-
-  def display_board
-    puts " #{board[0]} | #{board[1]} | #{board[2]}",
-         '-' * 11,
-         " #{board[3]} | #{board[4]} | #{board[5]}",
-         '-' * 11,
-         " #{board[6]} | #{board[7]} | #{board[8]}"
-  end
-
-  def welcome
-    puts 'Welcome to Tic Tac Toe'
-    puts ''
-  end
-
-  def player_setup
-    puts 'Player X please input your name'
-    name = gets.strip
-    @player1 = Player.new(name, 'X')
-    puts 'Player O please input your name'
-    name = gets.strip
-    until name != @player1.name
-      puts 'please input a different name, that name is already taken'
-      name = gets.strip
-    end
-    @player2 = Player.new(name, 'O')
-  end
-
-  def current_player
-    if @count.odd?
-      @player1
-    else
-      @player2
-    end
-  end
-
-#   def turn
-#     @game_over = false
-#     puts "#{current_player.name} it is your turn, please make your move choosing between 1 to 9"
-#   end
-
-  def input_board
-    move = gets.strip
-    until @game_status.valid_move?(move, @board)
-      puts 'Please enter a valid input between 1 to 9 that has not been taken'
-      move = gets.strip
-    end
-    @board[move.to_i - 1] = current_player.sign
-  end
-
-  def game_play
-    welcome
-    player_setup
-    display_board
-    loop do
-    #   turn
-      @game_over = false
-      puts "#{current_player.name} it is your turn, please make your move choosing between 1 to 9"
-      input_board
-      display_board
-
-      if @game_status.win_check(@board)
-        puts "#{current_player.name} is the winner"
-        return @game_over = true
-      end
-
-      if @game_status.draw?(@count)
-        puts 'The game is a draw'
-        return @game_over = true
-      end
-
-      break if @game_over
-
-      @count += 1
-      current_player
-    end
-
-
-    # turn until @game_over
-  end
-end
+require_relative '../lib/board'
 
 game_board = Board.new
-game_board.game_play
+puts 'Welcome to Tic Tac Toe'
+puts ''
+puts 'Player X please input your name'
+name = gets.strip
+@player1 = Player.new(name, 'X')
+puts 'Player O please input your name'
+name = gets.strip
+until name != @player1.name
+  puts 'please input a different name, that name is already taken'
+  name = gets.strip
+end
+@player2 = Player.new(name, 'O')
+puts game_board.display_board
+count = 1
 
-# game_board.welcome
-# game_board.player_setup
-# game_board.display_board
-# game_board.turn until @game_over
+loop do
+  game_over = false
+  game_status = GameStatus.new
+  to_play = Player.current_player(count)
+  puts "#{to_play.name} it is your turn, please make your move choosing between 1 to 9"
+  move = gets.strip
+  until game_status.valid_move?(move, game_board.board)
+    puts 'Please enter a valid input between 1 to 9 that has not been taken'
+    move = gets.strip
+  end
+  game_board.input_board(to_play, move)
+  puts game_board.display_board
+  if game_status.win_check(game_board.board)
+    puts "#{to_play.name} is the winner"
+    return game_over = true
+  end
+  if game_status.draw?(count) && !game_status.win_check(game_board.board)
+    puts 'The game is a draw'
+    return game_over = true
+  end
+  count += 1
+  break if game_over
+end
--- lib/players.rb
+++ lib/players.rb
@@ -1,8 +1,22 @@
 class Player
-  attr_reader :name, :sign
+  attr_reader :name, :sign, :player_list
+  @@player_list = []
 
   def initialize(name, sign)
     @name = name
     @sign = sign
+    @@player_list << self
+  end
+
+  def self.player_list
+    @@player_list
+  end
+
+  def self.current_player(count)
+    if count.odd?
+      Player.player_list[0]
+    else
+      Player.player_list[1]
+    end
   end
 end
